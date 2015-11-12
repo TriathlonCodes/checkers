24fbbee6862333b01925e0e84c9662a7304df84a
[01;31m[K"name"[m[K: "Stephanie Hutson <[01;31m[K"email"[m[K: null>
2015-11-11 17:17:40 -0800

    make most of the rules and set up the basic display

---
+++ application.js
@@ -0,0 +1,14 @@
+var game = new CheckersGame
+$(document).ready(function(){
+  displayChips()
+})
+
+
+var displayChips = function(){
+  $("td").empty()
+  for(var i = 0; i < 64; i ++){
+    if( game.board[i] != null ){
+      $("#loc-" + i).html(game.board[i])
+    }
+  }
+}
