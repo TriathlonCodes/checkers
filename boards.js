e208a32b634b233e7868087878ed2693f333bc9a
[01;31m[K"name"[m[K: "Stephanie Hutson <[01;31m[K"email"[m[K: null>
2015-11-22 15:01:36 -0500

    separate out the boards

---
+++ boards.js
@@ -0,0 +1,28 @@
+var r = "red"
+var b = "black"
+var checkerBoard = [b,null,b,null,b,null,b,null,
+          null,b,null,b,null,b,null,b,
+          b,null,b,null,b,null,b,null,
+          null,null,null,null,null,null,null,null,
+          null,null,null,null,null,null,null,null,
+          null,r,null,r,null,r,null,r,
+          r,null,r,null,r,null,r,null,
+          null,r,null,r,null,r,null,r]
+
+var blackBoard = [b,null,b,null,b,null,b,null,
+          null,b,null,b,null,b,null,b,
+          b,null,b,null,b,null,b,null,
+          null,null,null,null,null,null,null,null,
+          null,null,null,null,null,null,null,null,
+          null,null,null,null,null,null,null,null,
+          null,null,null,null,null,null,null,null,
+          null,null,null,null,null,null,null,null]
+
+var doubleBoard = [b,null,b,null,b,null,b,null,
+          null,b,null,b,null,b,null,b,
+          b,null,b,null,b,null,b,null,
+          null,null,null,null,null,r,null,null,
+          null,null,null,null,null,null,null,null,
+          null,null,null,r,null,null,null,null,
+          null,null,null,null,null,null,null,null,
+          null,null,null,null,null,null,null,null]
